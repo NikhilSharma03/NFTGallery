6c48526117840b1b4c629b7fbe26f0ca43a2f311
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2022-09-12 18:17:22 +0530

    added WalletModal

--- components/WalletModal/walletModal.style.ts
+++ components/WalletModal/walletModal.style.ts
@@ -1,10 +1,26 @@
 import styled from "styled-components";
-import { ModalContent } from "@chakra-ui/react";
+import { Box, Heading, ModalContent } from "@chakra-ui/react";
 
 export const ModalMain = styled(ModalContent)`
+  background: ${({ theme }) => theme.body};
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%) !important;
   margin: 0;
+`;
+
+export const WalletCard = styled(Box)`
+  border-width: 1px;
+  overflow: hidden;
+  height: 50vh;
+  padding: 2rem;
+  position: relative;
+  cursor: pointer;
+`;
+
+export const WalletHeading = styled(Heading)`
+  color: ${({ theme }) => theme.text};
+  text-align: center;
+  margin-top: 2rem;
 `;
--- components/WalletModal/walletModal.tsx
+++ components/WalletModal/walletModal.tsx
@@ -1,12 +1,7 @@
-import {
-  ModalOverlay,
-  ModalHeader,
-  Modal,
-  ModalCloseButton,
-  ModalBody,
-} from "@chakra-ui/react";
+import { ModalOverlay, Modal, ModalBody } from "@chakra-ui/react";
 import type { NextPage } from "next";
-import { ModalMain } from "./walletModal.style";
+import Image from "next/image";
+import { ModalMain, WalletCard, WalletHeading } from "./walletModal.style";
 
 interface Props {
   isOpen: boolean;
@@ -14,14 +9,24 @@ interface Props {
 }
 
 const WalletModal: NextPage<Props> = ({ isOpen, onClose }) => {
+  const onConnectWallet = () => {
+    onClose();
+  };
+
   return (
     <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalMain>
-        <ModalHeader>Connect</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          <h1>Body</h1>
+        <ModalBody padding="1rem">
+          <WalletCard maxW="lg" borderRadius="lg" onClick={onConnectWallet}>
+            <Image
+              alt="metamask"
+              src="/metamask.png"
+              width={4000}
+              height={4000}
+            />
+            <WalletHeading>Metamask</WalletHeading>
+          </WalletCard>
         </ModalBody>
       </ModalMain>
     </Modal>
